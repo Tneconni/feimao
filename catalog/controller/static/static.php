46a9b8b393f04972fd5b211b0306c79d88fbb151
cloud <792724991@qq.com>
2014-04-16 12:26:12 +0800

    add the static html file of 'print market'

--- catalog/controller/static/static.php
+++ catalog/controller/static/static.php
@@ -23,6 +23,24 @@ class ControllerStaticStatic extends Controller {
 
     }
 
+    public function print_market() {
 
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/static/print_market.tpl')) {
+            $this->template = $this->config->get('config_template') . '/template/static/print_market.tpl';
+        } else {
+            $this->template = 'default/template/static/print_market.tpl';
+        }
+
+        $this->children = array(
+
+            'common/footer',
+            'common/header'
+
+        );
+
+        $this->response->setOutput($this->render());
+
+    }
 
 }
